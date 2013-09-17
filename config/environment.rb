9e3d001ecc3408b8ee11ed26195761e7867e9ce8
Ho Tran Minh Khang <hotranminhkhang@gmail.com>
2013-09-17 19:40:24 +0700

    try 3 - change FROM emails to lisa@carpenta.vn

--- config/environment.rb
+++ config/environment.rb
@@ -24,6 +24,6 @@ ActionMailer::Base.smtp_settings = {
   :authentication => :plain,
   :user_name      => ENV['SENDGRID_USERNAME'],
   :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com',
+  :domain         => 'carpenta.com',
   :enable_starttls_auto => true
 }
