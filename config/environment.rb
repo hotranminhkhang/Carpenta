49970f15da23f7c1b6c03e33cbe51872c856a2ac
Ho Tran Minh Khang <hotranminhkhang@gmail.com>
2013-09-17 18:39:16 +0700

    Try 2 - Config mailer setup for heroku

--- config/environment.rb
+++ config/environment.rb
@@ -6,14 +6,18 @@ Carpenta::Application.initialize!
 
 
 Carpenta::Application.configure do
+	config.action_mailer.default_url_options = { :host => 'carpenta.herokuapp.com' }
 	config.action_mailer.delivery_method = :smtp
-
+	config.action_mailer.perform_deliveries = true
+	config.action_mailer.raise_delivery_errors = false
+	config.action_mailer.default :charset => "utf-8"
 	config.action_mailer.smtp_settings = {
-		address: 								"smtp.gmail.com",
-		port: 									587, 	
-		authentication: 				:login,
-		user_name: 							"hotranminhkhang@gmail.com",
-		password: 							"Neverb@ckd0wn1",
-		enable_starttls_auto: 	true
+	  address: "smtp.gmail.com",
+	  port: 587,
+	  domain: "carpenta.herokuapp.com",
+	  authentication: "plain",
+	  enable_starttls_auto: true,
+	  user_name: ENV["GMAIL_USERNAME"],
+	  password: ENV["GMAIL_PASSWORD"]
 	}
 end
